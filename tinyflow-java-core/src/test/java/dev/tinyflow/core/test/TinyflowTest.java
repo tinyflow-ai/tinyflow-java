a64a4f02f7ee9c2f907a75ff5da7d00e5ae8df00
Michael Yang <fuhai999@gmail.com>
2025-03-15 17:54:40 +0800

    test: update test

--- tinyflow-java-core/src/test/java/dev/tinyflow/core/test/TinyflowTest.java
+++ tinyflow-java-core/src/test/java/dev/tinyflow/core/test/TinyflowTest.java
@@ -1,5 +1,6 @@
 package dev.tinyflow.core.test;
 
+import com.agentsflex.core.chain.*;
 import com.agentsflex.llm.openai.OpenAILlm;
 import dev.tinyflow.core.Tinyflow;
 
@@ -8,16 +9,32 @@ import java.util.Map;
 
 public class TinyflowTest {
 
-//    static String data1 = "";
+    //    static String data1 = "";
     static String data1 = "{\"nodes\":[{\"id\":\"2\",\"type\":\"llmNode\",\"data\":{\"title\":\"大模型\",\"description\":\"处理大模型相关问题\",\"expand\":true,\"outputDefs\":[{\"id\":\"pyiig8ntGWZhVdVz\",\"dataType\":\"Object\",\"name\":\"param\",\"children\":[{\"id\":\"1\",\"name\":\"newParam1\",\"dataType\":\"String\"},{\"id\":\"2\",\"name\":\"newParam2\",\"dataType\":\"String\"}]}]},\"position\":{\"x\":600,\"y\":50},\"measured\":{\"width\":334,\"height\":687},\"selected\":false},{\"id\":\"3\",\"type\":\"startNode\",\"data\":{\"title\":\"开始节点\",\"description\":\"开始定义输入参数\",\"expand\":true,\"parameters\":[{\"id\":\"Q37GZ5KKvPpCD7Cs\",\"name\":\"name\"}]},\"position\":{\"x\":150,\"y\":25},\"measured\":{\"width\":306,\"height\":209},\"selected\":false},{\"id\":\"4\",\"type\":\"endNode\",\"data\":{\"title\":\"结束节点\",\"description\":\"结束定义输出参数\",\"expand\":true,\"outputDefs\":[{\"id\":\"z7fOwoTjQ7AbUJdm\",\"ref\":\"3.name\",\"name\":\"test\"}]},\"position\":{\"x\":994,\"y\":218},\"measured\":{\"width\":334,\"height\":209},\"selected\":false,\"dragging\":false}],\"edges\":[{\"markerEnd\":{\"type\":\"arrowclosed\",\"width\":20,\"height\":20},\"source\":\"3\",\"target\":\"2\",\"id\":\"xy-edge__3-2\"},{\"markerEnd\":{\"type\":\"arrowclosed\",\"width\":20,\"height\":20},\"source\":\"2\",\"target\":\"4\",\"id\":\"xy-edge__2-4\"}],\"viewport\":{\"x\":250,\"y\":100,\"zoom\":1}}";
+
     public static void main(String[] args) {
         Tinyflow tinyflow = new Tinyflow(data1);
         tinyflow.setLlmProvider(id -> OpenAILlm.of(""));
 
-        Map<String,Object> variables = new HashMap<>();
+        Map<String, Object> variables = new HashMap<>();
         variables.put("name", "michael");
 
-        Map<String, Object> result = tinyflow.toChain().executeForResult(variables);
+        Chain chain = tinyflow.toChain();
+        chain.addEventListener(new ChainEventListener() {
+            @Override
+            public void onEvent(ChainEvent event, Chain chain) {
+                System.out.println(event.toString());
+            }
+        });
+
+        chain.addOutputListener(new ChainOutputListener() {
+            @Override
+            public void onOutput(Chain chain, ChainNode node, Object outputMessage) {
+                System.out.println("outputMessage: " + outputMessage);
+            }
+        });
+
+        Map<String, Object> result = chain.executeForResult(variables);
 
         System.out.println(result);
     }
